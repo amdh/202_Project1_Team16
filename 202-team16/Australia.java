ddb25de8b3b29a28229432d794066f562bf20035
Jay Nirgudkar <jnirgudkar1@gmail.com>
2016-10-02 16:58:30 -0700

    Add files via upload

---
+++ 202-team16/Australia.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Australia here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Australia extends Place
+{
+    /**
+     * Act - do whatever the Australia wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ 202-team16/MumbaiIndia.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MumbaiIndia here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MumbaiIndia extends Place
+{
+    /**
+     * Act - do whatever the MumbaiIndia wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
