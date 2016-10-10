1d189a44f75e6851c401100c94a75ec8c08d834f
AdityaParashar93 <appadityaads@gmail.com>
2016-10-09 17:09:51 -0700

    MInor changes in classes-Image added for NYC-Message and shark classes created-

--- 202-team16/NewYork.java
+++ 202-team16/NewYork.java
@@ -12,8 +12,16 @@ public class NewYork extends Place
      * Act - do whatever the NewYork wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public NewYork(){
+
+        GreenfootImage image = new GreenfootImage("images/nyc.jpg");
+        image.scale(100,100);
+        //getWorld().getBackground().drawImage(image, 200,200);
+
+    }
+
     public void act() 
     {
-        // Add your action code here.
+        move(1);
     }    
 }
