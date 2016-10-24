2908844c4d2f462faab3e1fe5074a1bac52ecae9
jnirgudkar <jayajit.nirgudkar@sjsu.edu>
2016-10-24 01:11:52 -0700

    Australia class and loading the mumbai screen

--- 202-team16/MumbaiIndia.java
+++ 202-team16/MumbaiIndia.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
  * Write a description of class MumbaiIndia here.
  * 
@@ -12,17 +12,66 @@ public class MumbaiIndia extends Place implements PlaceInterface
      * Act - do whatever the MumbaiIndia wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
+    String backgroundImgPath;
+    String textHintPath;
+    String soundHintPath;
+    String imageHintpath ;
+    String answerOption1Path ;
+    String answerOption2Path;
+    String answerOption3Path ;
+    String answerOption4Path;
+
+    int life = 0;
+    int hint = 1;
+
+    AnswerOption ansOP1;
+    AnswerOption ansOP2;
+    AnswerOption ansOP3;
+    AnswerOption ansOP4;
+    AnswerOption hintImg;
+    PirateWorld world ;
     
     
     public MumbaiIndia(){
+        
+        textHintPath = "";
+        imageHintpath = "";
+        
+        backgroundImgPath = "images/Mumbai/Mumbackgroud.jpg";
+        textHintPath = "";
+        soundHintPath="";
+        imageHintpath = "/images/Australia/mumbai.jpg";
+        answerOption1Path = "images/Australia/MumIncorrectop2.jpg";
+        answerOption2Path = "images/Australia/mumcorrectop.jpg";
+        answerOption3Path = "images/Australia/Mumicorrectop3.jpg";
+        answerOption4Path = "images/Australia/Mumincorrectop1.jpg";
     
     }
     
     
-    public void draw()
-    {
-        
+   public void draw(){
+        world =  getWorldOfType(PirateWorld.class);
+        //world.removeObject(world.getObjects(StartGame.class).get(1));
+
+        world.setBackground(backgroundImgPath);
+
+        world.setLife();
+        world.setPirate();
+
+        List<Life> pirateLife = world.getLife();
+        life = pirateLife.size();
+
+        world.showHint1(textHintPath);
+        hint= hint+1;
+
+        ansOP1 = new  AnswerOption(answerOption1Path,false);
+        ansOP2 = new  AnswerOption(answerOption2Path,true);
+        ansOP3 = new  AnswerOption(answerOption3Path,false);
+        ansOP4 = new  AnswerOption(answerOption4Path,false);
+        world.setAnswerOptions(ansOP1,ansOP2,ansOP3,ansOP4);
+
+        showHurdle();
+        //showHurdle();
     }
     
     public void passedState()
@@ -34,4 +83,9 @@ public class MumbaiIndia extends Place implements PlaceInterface
     {
         // Add your action code here.
     }    
+    
+    public void showHurdle()
+    {
+    }
+    
 }
